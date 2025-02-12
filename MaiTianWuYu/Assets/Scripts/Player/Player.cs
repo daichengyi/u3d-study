1591eb48622701c12b5382e11db56354d9213a2b
daichengyi <443859613@qq.com>
2025-02-12 09:38:37 +0800

    05Player移动02

--- MaiTianWuYu/Assets/Scripts/Player/Player.cs
+++ MaiTianWuYu/Assets/Scripts/Player/Player.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D rb;
 
+    private float inputX;
+    private float inputY;
+    private Vector2 movementInput;
+
+    public float speed;
+
+    
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();//物理帧更新移动
+    }
+
+    private void PlayerInput()
+    {
+        // if(inputX == 0)
+        inputX = Input.GetAxisRaw("Horizontal");
+        // if(inputY == 0)
+        inputY = Input.GetAxisRaw("Vertical");
+
+        //修正斜方向移动速度
+        if (inputX != 0 && inputY != 0) {
+            inputX = inputX * 0.6f;
+            inputY = inputY * 0.6f;
+        }
+
+        movementInput = new Vector2(inputX, inputY);
+    }
+
+    private void Movement()
+    {
+        rb.MovePosition(rb.position + movementInput *speed * Time.deltaTime);
     }
 }
